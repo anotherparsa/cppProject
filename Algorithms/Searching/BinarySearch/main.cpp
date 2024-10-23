3012cecc7367e00842cab657e4a21862926592a0
anotherparsa <anotherparsa@gmail.com>
2024-10-23 06:22:23 +0330

    Binary Search

---
+++ Algorithms/Searching/BinarySearch/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int BinarySearch(int arr[], int size, int target);
+
+int main(){
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = (sizeof(arr) / sizeof(int));
+    cout << BinarySearch(arr, size, 5) << endl;
+}
+
+int BinarySearch(int arr[], int size, int target){
+    int start = 0 ;
+    int stop = size - 1;
+    while (start <= stop){
+        int middle = (start + stop ) / 2;
+        
+        if (arr[middle] == target){
+            return middle;
+        }else if (arr[middle] > target){
+            stop = middle - 1;
+        }else{
+            start = middle + 1;
+        }
+    }
+    return -1;
+}
