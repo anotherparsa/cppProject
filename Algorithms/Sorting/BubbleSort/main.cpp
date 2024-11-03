87c5b22c8709fd9a80f1f2c8a26e2e0e05d48938
anotherparsa <anotherparsa@gmail.com>
2024-11-03 05:15:25 +0330

    Bubble Sort

--- Algorithms/Sorting/BubbleSort/main.cpp
+++ Algorithms/Sorting/BubbleSort/main.cpp
@@ -2,41 +2,39 @@
 
 using namespace std;
 
-void bubbleSort(int arr[], int size);
-void swapping(int arr[], int a, int b);
-void showArray(int arr[], int size);
+void BubbleSort(int arr[], int size);
+void ShowArray(int arr[], int size);
+void Swapping(int arr[], int a, int b);
 
 int main(){
     int arr[] = {1, 9, 4, 2, 3, 6, 8, 7, 5};
     int size = (sizeof(arr) / sizeof(int));
-    cout << "Before bubble sorting: " << endl;
-    showArray(arr, size);
-    bubbleSort(arr,size);
-    cout << "After bubble sorting: " << endl;
-    showArray(arr, size);
-
+    cout << "Before Bubble Sorting: " << endl;
+    ShowArray(arr, size);
+    cout << "After Bubble Sorting: " << endl;
+    BubbleSort(arr, size);
+    ShowArray(arr, size);
 }
 
-void bubbleSort(int arr[], int size){
-    for (int i = 1 ; i <= size - 1; i++){
-        for (int j = 0 ; j < size - i; j++){
+void BubbleSort(int arr[], int size){
+    for (int i = 1 ; i < size - 2 ; i++){
+        for (int j = 0 ; j < size - i ; j++){
             if (arr[j] > arr[j + 1]){
-                swapping(arr, j, j+ 1);
+                Swapping(arr, j, j + 1);
             }
         }
     }
 }
 
-void swapping(int arr[], int a, int b){
-    int temp = arr[a];
-    arr[a] = arr[b];
-    arr[b] = temp;
-}
-
-void showArray(int arr[], int size){
-    cout << "Array Items : " << endl;
-    for (int i = 0 ; i < size ; i++ ){
+void ShowArray(int arr[], int size){
+    for (int i = 0 ; i < size ; i++){
         cout << arr[i] << " " ;
     }
     cout << endl;
+}
+
+void Swapping(int arr[], int a, int b){
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
 }
