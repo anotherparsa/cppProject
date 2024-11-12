5db059d14c42ed3d7dd7982e534a81e27b9936f5
anotherparsa <anotherparsa@gmail.com>
2024-11-12 14:12:11 +0330

    Print Stack

--- DataStructure/Stack/main.cpp
+++ DataStructure/Stack/main.cpp
@@ -9,18 +9,39 @@ class Node{
         Node(int value){
             this->value = value;
             this->next = nullptr;
+            
         }
 };
 
 class Stack{
     private:
-        int top;
-        int maxSize;
+        Node* top;
+        int height;
     
     public:
         Stack(int maxSize){
-            this->top = -1;
-            this->maxSize = maxSize;
+            this->top = nullptr;
+            this->height = 0;
         }
 
+        bool isEmpty(){
+            return (this->height == 0); 
+        }
+
+        void printStack(){
+            if (this->isEmpty()){
+                cout << "Stack is empty" << endl;
+            }else{
+                Node* temp = this->top;
+                while (temp){
+                    cout << temp->value << endl;
+                    temp = temp->next;
+                }
+            }
+        }
 };
+
+int main(){
+    Stack* myStack = new Stack(10);
+    myStack->printStack();
+}
