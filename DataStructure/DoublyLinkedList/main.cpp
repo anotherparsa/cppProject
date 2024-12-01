dd5901d857356727923e7e17e9ef85056eded1e8
anotherparsa <anotherparsa@gmail.com>
2024-12-01 10:15:29 +0330

    Doubly Linked List

---
+++ DataStructure/DoublyLinkedList/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* next;
+        Node* prev;
+
+        Node(int value){
+            this->value = value;
+            this->next = nullptr;
+            this->prev = nullptr;
+        }
+};
+
+class DoublyLinkedList{
+    private:
+        Node* head;
+        Node* tail;
+        int length;
+    
+    public:
+        DoublyLinkedList(){
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->length = 0;
+        }
+
+
+
+
+};
+
+
+int main(){
+    DoublyLinkedList* MyDoublyLinkedList = new DoublyLinkedList();
+}
