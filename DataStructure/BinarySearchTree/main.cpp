f37cfe726a028debaf3d704177f21b1dabc97bdd
anotherparsa <anotherparsa@gmail.com>
2024-12-03 02:01:41 +0330

    BST

---
+++ DataStructure/BinarySearchTree/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int value;
+        Node* left;
+        Node* right;
+
+        Node(int value){
+            this->value = value;
+            this->left = nullptr;
+            this->right = nullptr;
+        }
+ 
+};
+
+class BinarySearchTree{
+    private:
+        Node* root;
+    
+    public:
+        BinarySearchTree(){
+            this->root = null;
+        }
+
+        
+}
