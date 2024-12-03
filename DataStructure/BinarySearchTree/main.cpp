19c7dc714bd31fe2565b453780b69601091aa408
anotherparsa <anotherparsa@gmail.com>
2024-12-03 13:46:32 +0330

    Inserting Nodes in BST

--- DataStructure/BinarySearchTree/main.cpp
+++ DataStructure/BinarySearchTree/main.cpp
@@ -7,23 +7,56 @@ class Node{
         int value;
         Node* left;
         Node* right;
-
         Node(int value){
             this->value = value;
             this->left = nullptr;
             this->right = nullptr;
         }
- 
+
 };
 
 class BinarySearchTree{
     private:
         Node* root;
-    
     public:
         BinarySearchTree(){
             this->root = nullptr;
         }
 
+        void insert_node(int value){
+            Node* new_node = new Node(value);
+            if (this->root == nullptr){
+                this->root = new_node;
+                return;
+            }else{
+                Node* temp = this->root;
+                while(true){
+                    if (new_node->value == temp->value){
+                        return;
+                    }else{
+                        if(new_node->value < temp->value){
+                            if (temp->left == nullptr){
+                                temp->left == new_node;
+                                return;
+                            }else{
+                                temp = temp->left;
+                            }
+                        }else{
+                            if (temp->right == nullptr){
+                                temp->right = new_node;
+                                return;
+                            }else{
+                                temp = temp->right;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+};
+
+int main(){
 
 }
